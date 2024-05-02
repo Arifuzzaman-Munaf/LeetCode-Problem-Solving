b7a734bd86a18bb5d3e3bab281e3c57d175da9a5
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2024-05-02 23:25:52 +1000

    Solved 53. Maximum Subarray

---
+++ maximum_subarray.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = current_sum = 0
+
+        for i in nums:
+            if current_sum < 0:
+                current_sum = 0
+
+            current_sum += i
+            max_sum = max(current_sum, max_sum)
+
+        return max_sum
+
+
+print(Solution().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
