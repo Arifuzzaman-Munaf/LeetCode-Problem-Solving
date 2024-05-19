6d87996f40effd6ade527f58c357599121c16749
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2024-05-19 22:45:13 +1000

    Solved 338. Counting Bits

---
+++ counting_bits.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0]
+
+        for i in range(1, n + 1):
+            ans.append(ans[i >> 1] + (i & 1))
+
+        return ans
+
+
+print(Solution().countBits(4))
