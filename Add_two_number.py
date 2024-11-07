ad999700879de061e73096f5f1a478e715a28b43
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2024-11-07 15:24:47 +1100

    Solved 2. Add Two Numbers

--- Add_two_number.py
+++ Add_two_number.py
@@ -1,17 +1,35 @@
 # Definition for singly-linked list.
 class ListNode:
-     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode :
-        result = []
-        for i in range(len(l1)):
-            result.append(l1[i] + l2[i])
-            
-        print(result)
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        temp = ListNode()
+        result = temp
+        carry = 0
+        while l1 or l2 or carry:
+            total = carry
+            if l1:
+                total += l1.val
+                l1 = l1.next
+            if l2:
+                total += l2.val
+                l2 = l2.next
+            num = total % 10
+            carry = total // 10
+            temp.next = ListNode(num)
+            temp = temp.next
+        return result.next
 
 
-Solution().addTwoNumbers([1,2,3], [2,3,4])
+l1 = ListNode(1)
+l1.next = ListNode(8)
+l2 = ListNode(0)
 
-#munaf
+sol = Solution().addTwoNumbers(l1, l2)
+while sol:
+    print(sol.val)
+    sol = sol.next
